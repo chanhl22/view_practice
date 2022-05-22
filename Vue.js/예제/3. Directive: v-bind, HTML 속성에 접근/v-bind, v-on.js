7ef50ee33602bda97d01eec33db235482b2ec422
chanhl22 <lch9502@naver.com>
2022-05-22 19:53:51 +0900

    v-bind, v-on

---
+++ Vue.js/예제/3. Directive: v-bind, HTML 속성에 접근/v-bind, v-on.js
@@ -0,0 +1,41 @@
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        link: "https://github.com/chanhl22"
+    },
+    methods: {
+    }
+})
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        link: "https://github.com/chanhl22"
+    },
+    methods: {
+    }
+})
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        link: "https://github.com/chanhl22",
+    },
+    methods: {
+        move: function () {
+            location.href = "https://github.com/chanhl22"
+        }
+    }
+})
+
+var app4 = new Vue({
+    el: '#app4',
+    data: {
+        link: "https://github.com/chanhl22"
+    },
+    methods: {
+        move: function () {
+            location.href = "https://github.com/chanhl22"
+        }
+    }
+})
