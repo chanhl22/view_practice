3018040b36249093a32a759c64efa335d52ec743
chanhl22 <lch9502@naver.com>
2022-05-30 15:15:38 +0900

    v-model 바인딩

--- Vue.js/1. 시작하기/사용자 입력 핸들링/binding.js
+++ Vue.js/1. 시작하기/사용자 입력 핸들링/binding.js
@@ -2,5 +2,10 @@ var app6 = new Vue({
     el: '#app-6',
     data: {
         message: '안녕하세요 Vue!'
+    },
+    methods: {
+        test: function () {
+            console.log(this.message);
+        }
     }
 })
