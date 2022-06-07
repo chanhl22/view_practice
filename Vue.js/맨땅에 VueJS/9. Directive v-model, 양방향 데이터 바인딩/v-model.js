ca00dc2094ef815823c2685cef965e2e2035fbc2
chanhl22 <lch9502@naver.com>
2022-06-07 15:28:01 +0900

    v-model

--- Vue.js/맨땅에 VueJS/9. Directive v-model, 양방향 데이터 바인딩/v-model.js
+++ Vue.js/맨땅에 VueJS/9. Directive v-model, 양방향 데이터 바인딩/v-model.js
@@ -1,19 +1,6 @@
 new Vue({
     el: "#app",
     data: {
-        count: 0,
-        secondCount: 0
-    },
-    computed: {
-        double: function () {
-            console.log('Computed double')
-            return this.count * 2
-        }
-    },
-    methods: {
-        triple: function () {
-            console.log('methods triple')
-            return this.count * 3
-        }
+        name: "Hello"
     }
 })
