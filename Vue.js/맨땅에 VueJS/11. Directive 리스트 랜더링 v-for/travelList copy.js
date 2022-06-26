dfbc3203e54af1e40e54f493aea118339174bd98
chanhl22 <lch9502@naver.com>
2022-06-26 21:15:45 +0900

    test

---
+++ Vue.js/맨땅에 VueJS/11. Directive 리스트 랜더링 v-for/travelList copy.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#app",
+    data: {
+        travelList: [
+            { name: '강릉', distance: '차로 3시간' },
+            { name: '부산', distance: '차로 5시간' },
+            { name: '춘천', distance: '차로 2시간' },
+            { name: '대만', distance: '비행기로 3시간' },
+            { name: '필리핀', distance: '비행기로 5시간' },
+            { name: '스페인', distance: '비행기로 17시간' },
+        ]
+    }
+})
